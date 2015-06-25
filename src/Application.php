35dd6c798726df37c419136896c8e2a08b2061a9
Phil Elson <phil@pegasus-commerce.com>
2015-06-25 07:40:28 +0100

    Updated the Application.php to use a more robust method of finding the autoload.php file

--- src/Application.php
+++ src/Application.php
@@ -27,13 +27,12 @@
  * Time: 12:50
  */
 
-if (file_exists(__DIR__.'/../vendor/autoload.php'))
-{
-    require_once __DIR__.'/../vendor/autoload.php';
-}
-else
-{
-    require_once 'phar://sanitizer.phar/vendor/autoload.php';
+if (file_exists(__DIR__.'/../vendor/autoload.php')) {
+    include_once __DIR__.'/../vendor/autoload.php';
+} else if (file_exists(__DIR__.'/../../../autoload.php')) {
+    include_once __DIR__.'/../../../autoload.php';
+} else {
+    include_once 'phar://sanitizer.phar/vendor/autoload.php';
 }
 
 
